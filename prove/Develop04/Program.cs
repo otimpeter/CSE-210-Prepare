6124436728a531862058df91b49d83dc6b0e8ce8
Peter Charles Otim <113760237+otimpeter@users.noreply.github.com>
2023-01-28 20:29:22 +0300

    commit updates

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,10 +1,6 @@
-
-
 using System;
-
 class Activity
 {
-
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
@@ -33,13 +29,13 @@ class BreathingActivity : Activity
     {
         base.Start();
         Console.WriteLine("Preparing to begin...");
-        Thread.Sleep(3000);
+        Thread.Sleep(30);
         for (int i = Duration; i > 0; i--)
         {
             Console.WriteLine("Breathe in...");
-            Thread.Sleep(1000);
+            Thread.Sleep(10);
             Console.WriteLine("Breathe out...");
-            Thread.Sleep(1000);
+            Thread.Sleep(10);
         }
     }
 }
@@ -50,8 +46,8 @@ class ReflectionActivity : Activity
     {
         "Think of a time when you stood up for someone else.",
         "Think of a time when you did something really difficult.",
-        "Think of a time when you helped someone in need.",
-        "Think of a time when you did something truly selfless."
+       
+       
     };
 
     private List<string> questions = new List<string>()
@@ -61,34 +57,29 @@ class ReflectionActivity : Activity
         "How did you get started?",
         "How did you feel when it was complete?",
         "What made this time different than other times when you were not as successful?",
-        "What is your favorite thing about this experience?",
-        "What could you learn from this experience that applies to other situations?",
-        "What did you learn about yourself through this experience?",
-        "How can you keep this experience in mind in the future?"
     };
 
     public override void Start()
     {
         base.Start();
         Console.WriteLine("Preparing to begin...");
-        Thread.Sleep(3000);
+        Thread.Sleep(30);
         Random rnd = new Random();
         int remainingTime = Duration;
         while (remainingTime > 0)
         {
             int questionIndex = rnd.Next(questions.Count);
             Console.WriteLine(prompts[questionIndex]);
-            Thread.Sleep(3000);
+            Thread.Sleep(30);
             Console.WriteLine(questions[questionIndex]);
             remainingTime -= 5;
-            Thread.Sleep(2000);
+            Thread.Sleep(20);
         }
     }
 }
 
 class ListingActivity : Activity
-{
-    private List<string> prompts = new List<string>()
+{    private List<string> prompts = new List<string>()
     {
         "Who are people that you appreciate?",
         "What are personal strengths of yours?",
@@ -99,4 +90,5 @@ class ListingActivity : Activity
 
     public
 
-}
+};
+
